c7e1a1c8564afbd52e47642e1ccbcf03be75367d
TailongCheng <157655951+TailongCheng@users.noreply.github.com>
2024-02-23 15:01:00 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const db = require('./persistence');
+const getItems = require('./routes/getItems');
+const addItem = require('./routes/addItem');
+const updateItem = require('./routes/updateItem');
+const deleteItem = require('./routes/deleteItem');
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+app.use(express.static(__dirname + '/static'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+app.get('/items', getItems);
+app.post('/items', addItem);
+app.put('/items/:id', updateItem);
+app.delete('/items/:id', deleteItem);
+
+db.init().then(() => {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
 });
 
-const PORT = process.env.PORT || 3000;
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit());
+};
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
